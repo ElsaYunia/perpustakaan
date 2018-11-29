9652287ef567d4cae36bd44dfa94adb67c380425
ELSA YUNIA <ELSA YUNIA>
2018-11-29 19:20:25 +0700

    tambah halaman kontak & struktur

---
+++ struktur.php
@@ -0,0 +1,352 @@
+<?php include "conn.php";
+ ?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Graha Baca Online</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--Google Font link-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+
+        <link rel="stylesheet" href="assets/css/slick/slick.css"> 
+        <link rel="stylesheet" href="assets/css/slick/slick-theme.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/iconfont.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/content_n.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="assets/css/bootsnav.css">
+
+        <!-- xsslider slider css -->
+
+
+        <!--<link rel="stylesheet" href="assets/css/xsslider.css">-->
+
+
+
+
+        <!--For Plugins external css-->
+        <!--<link rel="stylesheet" href="assets/css/plugins.css" />-->
+
+        <!--Theme custom css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!--<link rel="stylesheet" href="assets/css/colors/maron.css">-->
+
+        <!--Theme Responsive css-->
+        <link rel="stylesheet" href="assets/css/responsive.css" />
+
+        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+    <body data-spy="scroll" data-target=".navbar-collapse">
+
+
+        <!-- Preloader -->
+        <div id="loading">
+            <div id="loading-center">
+                <div id="loading-center-absolute">
+                    <div class="object" id="object_one"></div>
+                    <div class="object" id="object_two"></div>
+                    <div class="object" id="object_three"></div>
+                    <div class="object" id="object_four"></div>
+                </div>
+            </div>
+        </div><!--End off Preloader -->
+
+
+        <div class="culmn">
+            <!--Home page style-->
+
+
+            <nav class="navbar navbar-default bootsnav navbar-fixed">
+                <div class="navbar-top bg-grey fix">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="navbar-callus text-left sm-text-center">
+                                    <ul class="list-inline">
+                                        <li><a href=""><i class="fa fa-phone"></i> Call us: 0272 122 344</a></li>
+                                        <li><a href=""><i class="fa fa-envelope-o"></i> Contact us: kelompokweb497@gmail.com</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="navbar-socail text-right sm-text-center">
+                                    <ul class="list-inline">
+                                        <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Start Top Search -->
+                <div class="top-search">
+                    <div class="container">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                            <input type="text" class="form-control" placeholder="Search">
+                            <span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Top Search -->
+
+
+                <div class="container"> 
+                    <div class="attr-nav">
+                        <ul>
+                            <li class="search"><a href="#"><i class="fa fa-search"></i></a></li>
+                        </ul>
+                    </div> 
+
+                    <!-- Start Header Navigation -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <a class="nav-logo" href="#brand">
+                            <img src="assets/images/company.png" class="logo" alt="">
+                        </a>
+
+                    </div>
+                    <!-- End Header Navigation -->
+
+                    <!-- navbar menu -->
+                    <div class="collapse navbar-collapse" id="navbar-menu">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="index.php">Home</a></li>                    
+                            <li><a href="koleksi.php">Koleksi</a></li>
+                            <li class="dropdown">
+                             <a class="dropdown-toggle" data-toggle="dropdown" href="#">Tentang
+                            </a>
+                            <ul class="dropdown-menu">
+                            <li><a href="profil.php">Profil</a></li>
+                            <li><a href="tujuan.php">Tujuan, Visi & Misi</a></li>
+                            <li><a href="struktur.php">Struktur Organisasi</a></li>
+                            <li><a href="kontak.php">Kontak</a></li>
+                        </ul>
+                     </li>
+                            <li><a href="ketentuan.php">Ketentuan</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div> 
+            </nav>
+            </div>
+
+            <!--Body-->
+
+            <hr>
+            <hr>
+            <hr>
+            <hr>
+            <hr>
+            <hr>
+            <hr>
+            <div id="contentpane" class="article">
+                <div class="container">
+                 <div class="heading">
+                <h2 class="bebas">Struktur Organisasi Graha Baca Online</h2>
+           </div>
+        <hr/>
+                <h3> Kepala: Dra. Labibah, MLIS.
+Wakil Kepala                           : Dra. Khusnul Khotimah, SS., M.IP
+Kepala Bidang
+Kabid Layanan Pemustaka                   : Sri Astuti, SIP. M.IP
+Kabid Layanan Teknis                            : Dra. Ida Nur’aini Hadna, M.Pd.
+Kabid Layanan Teknologi Informasi    : Drs. Bambang Heru Nurwoto
+Kepala Urusan
+Kaur Umum dan Kepegawaian :  Agung Aridunta Herumurti, SH.
+Staf :
+Ismiyatin, S.Pd
+Asih Hidayati Yuli Astari, SE
+Minto Wasono
+Kaur Informasi : Isrowiyanti, S.Ag., SS.
+Staf :
+Puji Hartati, S.IP
+Basuki effendi. S.Sos
+Nur Arifin
+Kaur Pengembangan dan Pemeliharaan Koleksi : Siti Pamularsih, S.Sos., S.IPI
+Staf :
+Nur Akhmad
+Rohyati
+M. Syamsuddin
+Suhardi Widodo
+Kaur Sistem Informasi dan Jaringan : Edi Prasetya, S.Kom
+Staf :
+Fatchul Hijrih, S.Kom.
+Miftakhul Yazid Fuadi, SIP.
+Kaur Pengolahan Bahan Pustaka : Dra. Irhamny
+Staf :
+Drs. Moh. Tantowi, M.Si
+Muhammad Warsun, S.Pdi
+Kaur Repository Digital : Sugeng Harianto, SIP.
+Staf :
+Miftahul Ulum, S.Kom
+Zaenal Arifin, S.Sos.I., S.IPI.
+Kaur  Referensi dan Serial : Wahyani, S.Ag., M.IP
+Staf  :
+Hj. Dra. Retno Wuri W
+Etty Nurhayati
+Kaur Sirkulasi : Sri Lestari, M.IP
+Staf :
+Harniyati, S.H
+Suhardi, S.Sos
+Wiedyastuti Kartini, S.Sos
+Triono
+Ngadiman
+Badriyah
+                <p>&nbsp;</p>
+        </div>
+        </div>
+
+
+
+
+            <!--End Body-->
+
+
+
+
+            <footer id="contact" class="footer action-lage bg-black p-top-80">
+                <!--<div class="action-lage"></div>-->
+                <div class="container">
+                    <div class="row">
+                        <div class="widget_area">
+                            <div class="col-md-3">
+                                <div class="widget_item widget_about">
+                                    <h5 class="text-white">About Us</h5>
+                                    <p class="m-top-20">E-Library yang menyediakan bacaan buku terlengkap di Yogyakarta. Dengan lebih dari 1.000.000 Buku dan 100 member.</p>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-location-arrow"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">Location</h6>
+                                            <p>
+                                                Jalan Raya Janti, Bantul, Yogyakarta.</p>
+                                        </div>
+                                    </div>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-phone"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">Phone :</h6>
+                                            <p>0272 122 344</p>
+                                        </div>
+                                    </div>
+                                    <div class="widget_ab_item m-top-30">
+                                        <div class="item_icon"><i class="fa fa-envelope-o"></i></div>
+                                        <div class="widget_ab_item_text">
+                                            <h6 class="text-white">Email Address :</h6>
+                                            <p>webkelompok497@gmail.com</p>
+                                        </div>
+                                    </div>
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+
+                            <div class="col-md-3">
+                                <div class="widget_item widget_latest sm-m-top-50">
+                                    <h5 class="text-white">Agenda</h5>
+                                    <div class="widget_latst_item m-top-30">
+                                        <div class="item_icon"><img src="assets/images/agenda1.png" alt="" /></div>
+                                        <div class="widget_latst_item_text">
+                                            <p>Kunjungan Menristek</p>
+                                            <a href="">31<sup>th</sup> Des 2018</a>
+                                        </div>
+                                    </div>
+                                    <div class="widget_latst_item m-top-30">
+                                        <div class="item_icon"><img src="assets/images/agenda2.png" alt="" /></div>
+                                        <div class="widget_latst_item_text">
+                                            <p>Book Store</p>
+                                            <a href="">01<sup>th</sup> Jan 2019</a>
+                                        </div>
+                                    </div>
+                                    <div class="widget_latst_item m-top-30">
+                                        <div class="item_icon"></div>
+                                        <div class="widget_latst_item_text">
+                                        </div>
+                                    </div>
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+
+                            <div class="col-md-3">
+                                <div class="widget_item widget_service sm-m-top-50">
+                                    <h5 class="text-white">JAM LAYANAN</h5>
+                                    <ul class="m-top-20">
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> SENIN - KAMIS</a> <p>Buka jam 08.00 WIB - 19.00 WIB</p> </li>
+
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> JUM'AT</a><p>Buka jam 09.00 WIB - 19.30 WIB</p><p>ISTIRAHAT JAM 11.30-13.00 WIB</p></li>
+                                        <li class="m-top-20"><a href=""><i class="fa fa-angle-right"></i> SABTU - MINGGU</a><p>Buka jam 09.00 WIB - 14.00 WIB</p></li>
+                                    </ul>
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+
+                            <div class="col-md-3">
+                                <div class="widget_item widget_newsletter sm-m-top-50">
+                                    <h5 class="text-white">Newsletter</h5>
+                                    <form class="form-inline m-top-30">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" placeholder="Enter you Email">
+                                            <button type="submit" class="btn text-center"><i class="fa fa-arrow-right"></i></button>
+                                        </div>
+
+                                    </form>
+                                    <div class="widget_brand m-top-40">
+                                        <a href="" class="text-uppercase">GRAHA BACA ONLINE</a>
+                                        <p>Follow Us</p>
+                                    </div>
+                                    <ul class="list-inline m-top-20">
+                                        <li>-  <a href=""><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                                    </ul>
+
+                                </div><!-- End off widget item -->
+                            </div><!-- End off col-md-3 -->
+                        </div>
+                    </div>
+                </div>
+                <div class="main_footer fix bg-mega text-center p-top-40 p-bottom-30 m-top-80">
+                    <div class="col-md-12">
+                        <p class="wow fadeInRight" data-wow-duration="1s">
+                            © Copyright 2018 <span>GrahaBacaOnline</span>. Designed by GrahaBacaOnline. 
+                        </p>
+                    </div>
+                </div>
+            </footer>
+
+
+
+
+        </div>
+
+        <!-- JS includes -->
+
+        <script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
+        <script src="assets/js/vendor/bootstrap.min.js"></script>
+
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/jquery.magnific-popup.js"></script>
+        <script src="assets/js/jquery.easing.1.3.js"></script>
+        <script src="assets/css/slick/slick.js"></script>
+        <script src="assets/css/slick/slick.min.js"></script>
+        <script src="assets/js/jquery.collapse.js"></script>
+        <script src="assets/js/bootsnav.js"></script>
+
+
+
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
